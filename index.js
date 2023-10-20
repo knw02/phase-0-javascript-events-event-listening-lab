69e6dc3565707af777adc1c418120f5dd4581dc6
Kalie <kwatsonnov2002@gmail.com>
2023-10-20 01:52:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    console.log(input); // Add this line to check if the element is found
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
 }
